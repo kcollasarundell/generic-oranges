4c716207832b7168717bc9025fac52360f497d64
Kevin Collas-Arundell <me@kca.id.au>
2021-02-14 20:14:56 +1100

    fixing format

--- core/role/role.tf
+++ core/role/role.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_policy" "deployer" {
   name        = "Deployer"
-  path        = "kca/"
+  path        = "/kca/"
   description = "Core deployer policy to manage and restrict this deployer"
   policy = data.aws_iam_policy_document.deployer.json
 }
