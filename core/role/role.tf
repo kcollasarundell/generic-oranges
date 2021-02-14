4bf094f9a0d41acbee3a135aa6071b674a5edd0d
Kevin Collas-Arundell <me@kca.id.au>
2021-02-14 20:16:43 +1100

    add assume role permissions for 2 hardcoded users for now

--- core/role/role.tf
+++ core/role/role.tf
@@ -4,3 +4,24 @@ resource "aws_iam_policy" "deployer" {
   description = "Core deployer policy to manage and restrict this deployer"
   policy = data.aws_iam_policy_document.deployer.json
 }
+
+data "aws_iam_policy_document" "deployer" {
+  statement {
+    sid    = ""
+    effect = "Allow"
+    actions = [
+        "sts:AssumeRole",
+        ]
+            principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::234158512104:user/github-admin", "arn:aws:iam::234158512104:user/kca" ]
+    }
+  }
+}
+
+resource "aws_iam_role" "deploy-core" {
+    name = "deploy-core"
+    path = "/kca/core/"
+    description = "Core Deploy role"
+    assume_role_policy = ""
+}
