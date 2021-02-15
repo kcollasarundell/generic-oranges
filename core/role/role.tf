e343e51a72c4b0a3976307df8b234f87b8c1a429
Kevin Collas-Arundell <me@kca.id.au>
2021-02-15 21:03:53 +1100

    restructure policy and role attachments to isolate tfstate

--- core/role/role.tf
+++ core/role/role.tf
@@ -1,27 +1,66 @@
-resource "aws_iam_policy" "deployer" {
-  name        = "Deployer"
-  path        = "/kca/"
-  description = "Core deployer policy to manage and restrict this deployer"
-  policy = data.aws_iam_policy_document.deployer.json
+resource "aws_iam_role" "tfstate" {
+  name               = "tfstate"
+  path               = "/kca/core/"
+  description        = "Core state role"
+  assume_role_policy = data.aws_iam_policy_document.assumption.json
 }
 
-data "aws_iam_policy_document" "deploy_assume" {
+resource "aws_iam_role_policy_attachment" "tfstate_attach" {
+  role       = aws_iam_role.tfstate.name
+  policy_arn = aws_iam_policy.tfstate.arn
+}
+
+resource "aws_iam_role" "deploy_core" {
+  name               = "deploycore"
+  path               = "/kca/core/"
+  description        = "Core Deploy role"
+  assume_role_policy = data.aws_iam_policy_document.assumption.json
+}
+output "deploy_role" {
+  value = aws_iam_role.deploy_core.arn
+}
+resource "aws_iam_role_policy_attachment" "deploy_attach" {
+  role       = aws_iam_role.tfstate.name
+  policy_arn = aws_iam_policy.tfstate.arn
+}
+
+resource "aws_iam_policy" "deploy" {
+  name        = "deploy_role"
+  path        = "/kca/"
+  description = "Core deployer policy to manage and restrict this deployer"
+  policy      = data.aws_iam_policy_document.deploy.json
+}
+resource "aws_iam_policy" "tfstate" {
+  name        = "tf-state"
+  path        = "/kca/core/"
+  description = "IAM policy for state access"
+  policy      = data.aws_iam_policy_document.tfstate.json
+}
+
+# Manual application of state policy
+
+resource "aws_iam_user_policy_attachment" "attach_deployer" {
+  user       = "github-admin"
+  policy_arn = aws_iam_policy.tfstate.arn
+}
+resource "aws_iam_user_policy_attachment" "attach_kca" {
+  user       = "kca"
+  policy_arn = aws_iam_policy.tfstate.arn
+}
+
+data "aws_iam_policy_document" "assumption" {
   statement {
     sid    = ""
     effect = "Allow"
     actions = [
-        "sts:AssumeRole",
-        ]
-            principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::234158512104:user/github-admin", "arn:aws:iam::234158512104:user/kca" ]
+      "sts:AssumeRole",
+    ]
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::234158512104:user/github-admin",
+        "arn:aws:iam::234158512104:user/kca",
+      ]
     }
   }
-}
-
-resource "aws_iam_role" "deploy-core" {
-    name = "deploy-core"
-    path = "/kca/core/"
-    description = "Core Deploy role"
-    assume_role_policy = data.aws_iam_policy_document.deploy_assume.json
 }
