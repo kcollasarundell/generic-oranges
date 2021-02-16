20aad2059facad30a8587f72870329012b3e6b6d
Kevin Collas-Arundell <me@kca.id.au>
2021-02-17 06:44:50 +1100

    ensure policy applies on correct resources

--- core/role/policy.tf
+++ core/role/policy.tf
@@ -154,6 +154,7 @@ data "aws_iam_policy_document" "deploy_vpc" {
       "arn:aws:ec2:*:${data.aws_caller_identity.current.account_id}:route-table/*",
       "arn:aws:ec2:*:${data.aws_caller_identity.current.account_id}:elastic-ip/*", 
       "arn:aws:ec2:*:${data.aws_caller_identity.current.account_id}:security-group/*",
+      "arn:aws:ec2:*:${data.aws_caller_identity.current.account_id}:internet-gateway/*",
     ]
 
     actions = [
