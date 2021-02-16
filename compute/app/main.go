8f2b0f55be3ffa45a0edccbf5f8e484708413eb6
Kevin Collas-Arundell <me@kca.id.au>
2021-02-16 20:26:31 +1100

    Create go app

---
+++ compute/app/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", HelloServer)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
