6b48906d1619e72565b6753ca99a213200304f15
Kevin Collas-Arundell <me@kca.id.au>
2021-02-17 05:39:00 +1100

    Policy test for restricted user

--- core/role/policy.tf
+++ core/role/policy.tf
@@ -35,8 +35,8 @@ data "aws_iam_policy_document" "tfstate" {
     resources = ["*"]
 
     actions = [
-     
-      
+
+
       # User perms
       "iam:AttachUserPolicy",
       "iam:DetachUserPolicy",
@@ -89,6 +89,19 @@ data "aws_iam_policy_document" "deploy" {
     ]
 
     actions = [
+      #
+      "ec2:CreateVpc",
+      "ec2:CreateSubnet",
+      "ec2:DescribeAvailabilityZones",
+
+      "ec2:CreateRouteTable",
+      "ec2:CreateRoute",
+      "ec2:CreateInternetGateway",
+
+      "ec2:AttachInternetGateway",
+      "ec2:AssociateRouteTable",
+      "ec2:ModifyVpcAttribute",
+
       "ec2:AttachInternetGateway",
       "ec2:CreateNatGateway",
       "ec2:CreateRouteTable",
@@ -98,6 +111,60 @@ data "aws_iam_policy_document" "deploy" {
       "ec2:DeleteTags",
       "ec2:DescribeVpcClassicLink",
       "ec2:DescribeAccountAttributes",
+    ]
+  }
+
+  statement {
+    sid    = ""
+    effect = "Allow"
+
+    resources = ["*"]
+    actions = [
+      "ec2:DescribeAccountAttributes",
+      "ec2:DescribeAddresses",
+      "ec2:DescribeAvailabilityZones",
+      "ec2:DescribeClassicLinkInstances",
+      "ec2:DescribeClientVpnEndpoints",
+      "ec2:DescribeCustomerGateways",
+      "ec2:DescribeDhcpOptions",
+      "ec2:DescribeEgressOnlyInternetGateways",
+      "ec2:DescribeFlowLogs",
+      "ec2:DescribeInternetGateways",
+      "ec2:DescribeManagedPrefixLists",
+      "ec2:DescribeMovingAddresses",
+      "ec2:DescribeNatGateways",
+      "ec2:DescribeNetworkAcls",
+      "ec2:DescribeNetworkInterfaceAttribute",
+      "ec2:DescribeNetworkInterfacePermissions",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DescribePrefixLists",
+      "ec2:DescribeRouteTables",
+      "ec2:DescribeSecurityGroupReferences",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeStaleSecurityGroups",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeTags",
+      "ec2:DescribeTrafficMirrorFilters",
+      "ec2:DescribeTrafficMirrorSessions",
+      "ec2:DescribeTrafficMirrorTargets",
+      "ec2:DescribeTransitGatewayRouteTables",
+      "ec2:DescribeTransitGateways",
+      "ec2:DescribeTransitGatewayVpcAttachments",
+      "ec2:DescribeVpcAttribute",
+      "ec2:DescribeVpcClassicLink",
+      "ec2:DescribeVpcClassicLinkDnsSupport",
+      "ec2:DescribeVpcEndpointConnectionNotifications",
+      "ec2:DescribeVpcEndpointConnections",
+      "ec2:DescribeVpcEndpoints",
+      "ec2:DescribeVpcEndpointServiceConfigurations",
+      "ec2:DescribeVpcEndpointServicePermissions",
+      "ec2:DescribeVpcEndpointServices",
+      "ec2:DescribeVpcPeeringConnections",
+      "ec2:DescribeVpcs",
+      "ec2:DescribeVpnConnections",
+      "ec2:DescribeVpnGateways",
+      "ec2:GetManagedPrefixListAssociations",
+      "ec2:GetManagedPrefixListEntries"
     ]
   }
 
