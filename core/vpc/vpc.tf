011ae2074cd5289f12f3a91ee7afd5a7fb845d2f
Kevin Collas-Arundell <me@kca.id.au>
2021-02-17 05:48:33 +1100

    shrink to single nat gateway

--- core/vpc/vpc.tf
+++ core/vpc/vpc.tf
@@ -9,7 +9,7 @@ resource "aws_route53_zone" "primary" {
 }
 
 output "name_servers" {
-  value = aws_route53_zone.primary.name_servers
+    value = aws_route53_zone.primary.name_servers
 }
 
 # This upstream module is maintained by a aws community hero and the automates a large amount of the details involved in a vpc
@@ -22,9 +22,10 @@ module "prod" {
 
   azs            = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
   public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 
   enable_nat_gateway = true
-  single_nat_gateway = false
+  single_nat_gateway = true
 
   tags = {
     Terraform   = "true"
@@ -40,9 +41,10 @@ module "dev" {
 
   azs            = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
   public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 
   enable_nat_gateway = true
-  single_nat_gateway = false
+  single_nat_gateway = true
 
   tags = {
     Terraform   = "true"
