03859b0a3799d0daf5c745a49635e8847ba047df
Kevin Collas-Arundell <me@kca.id.au>
2021-02-19 09:47:36 +1100

    restructure permissions

--- core/app/deploy.tf
+++ core/app/deploy.tf
@@ -82,22 +82,32 @@ data "aws_iam_policy_document" "deployer" {
       "cloudwatch:DescribeAlarms",
     ]
   }
-
   statement {
-    sid    = ""
+    sid    = "DNS write"
+    effect = "Allow"
+    resources = [
+      "arn:aws:route53:::hostedzone/${data.aws_route53_zone.generic_oranges.zone_id}",
+    ]
+
+    actions = [
+      "route53:ChangeResourceRecordSets",
+    ]
+  }
+  statement {
+    sid    = "DNSRead"
     effect = "Allow"
     resources = [
       "arn:aws:route53:::hostedzone/*",
     ]
 
     actions = [
-      "route53:GetHostedZone",
-      "route53:ListResourceRecordSets",
-      "route53:ListHostedZones",
-      "route53:ChangeResourceRecordSets",
-      "route53:ListResourceRecordSets",
-      "route53:GetHostedZoneCount",
-      "route53:ListHostedZonesByName"
+      "route53:Get*",
+      "route53:List*",
     ]
   }
+}
+
+
+data "aws_route53_zone" "generic_oranges" {
+  name = "generic-oranges.dev."
 }
