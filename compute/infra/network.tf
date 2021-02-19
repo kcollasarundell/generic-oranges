df8b921db72f877962c25b7abbd39fe01fbdb30a
Kevin Collas-Arundell <me@kca.id.au>
2021-02-19 13:18:17 +1100

    add dns zone

--- compute/infra/network.tf
+++ compute/infra/network.tf
@@ -16,7 +16,7 @@ resource "aws_lb_target_group" "oranges" {
   name     = "oranges-worker"
   port     = 8080
   protocol = "HTTP"
-  vpc_id   = aws_vpcs.prod_oranges.ids
+  vpc_id   = data.aws_vpcs.prod_oranges.ids
 }
 
 resource "aws_lb_listener" "oranges" {
@@ -32,7 +32,7 @@ resource "aws_lb_listener" "oranges" {
 resource "aws_security_group" "ALB" {
   name        = "ALB"
   description = "Allow http(s) ingress"
-  vpc_id      = aws_vpcs.prod_oranges.ids
+  vpc_id      = data.aws_vpcs.prod_oranges.ids
 
   ingress {
     description = "https ingress to ALB"
@@ -76,9 +76,7 @@ resource "aws_security_group" "asg_ingress" {
 resource "aws_security_group" "asg_egress" {
   name        = "asg_egress"
   description = "Allow outbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-
+  vpc_id      = data.aws_vpcs.prod_oranges.ids
 
   egress {
     from_port   = 53
@@ -97,5 +95,35 @@ resource "aws_security_group" "asg_egress" {
   }
   tags = {
     Name = "worker-egress"
+  }
+}
+
+data "aws_route53_zone" "oranges" {
+  name  = "generic-oranges.dev."
+}
+
+resource "aws_route53_record" "all" {
+  zone_id = aws_route53_zone.oranges.zone_id
+  name    = "*.generic-oranges.dev."
+  type    = "A"
+  ttl     = "300"
+
+  alias {
+    name                   = aws_lb.oranges.dns_name
+    zone_id                = aws_lb.oranges.zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "root" {
+  zone_id = aws_route53_zone.oranges.zone_id
+  name    = "generic-oranges.dev."
+  type    = "A"
+  ttl     = "300"
+
+  alias {
+    name                   = aws_lb.oranges.dns_name
+    zone_id                = aws_lb.oranges.zone_id
+    evaluate_target_health = false
   }
 }
