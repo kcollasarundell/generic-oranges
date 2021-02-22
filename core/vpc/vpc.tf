99a68348c0e4e6cf0e6ff274d9d5cd69dda6de8f
Kevin Collas-Arundell <me@kca.id.au>
2021-02-22 19:01:46 +1100

    Ensure subnets are created and available with ipv6 connectivity

--- core/vpc/vpc.tf
+++ core/vpc/vpc.tf
@@ -15,8 +15,8 @@ module "prod" {
   name = "prod-oranges"
   cidr = "10.0.0.0/16"
 
-  azs            = ["ap-southeast-2a", ]
-  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24" ]
+  azs            = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c", ]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   public_subnet_tags = {
     tier = "public"
   }
@@ -25,15 +25,16 @@ module "prod" {
   }
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 
-  enable_ipv6                     = true
-  assign_ipv6_address_on_creation = true
+  enable_ipv6                                    = true
+  assign_ipv6_address_on_creation                = true
+  private_subnet_assign_ipv6_address_on_creation = true
 
-  private_subnet_assign_ipv6_address_on_creation = false
 
-  public_subnet_ipv6_prefixes   = [10,11,12]
-  private_subnet_ipv6_prefixes  = [13,14,15]
-  enable_nat_gateway = false
-  single_nat_gateway = false
+  public_subnet_ipv6_prefixes  = [10, 11, 12]
+  private_subnet_ipv6_prefixes = [13, 14, 15]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
   tags = {
     Terraform   = "true"
@@ -57,8 +58,7 @@ module "dev" {
   private_subnet_tags = {
     tier = "private"
   }
-
-
+  enable_ipv6        = false
   enable_nat_gateway = true
   single_nat_gateway = true
 
